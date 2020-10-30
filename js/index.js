7802b7630f8b6b0ec926e7e3a95b5edb169708df
Manar <manarshaar7@gmail.com>
2020-10-30 21:55:47 +0200

    Task Done!

--- js/index.js
+++ js/index.js
@@ -43,3 +43,63 @@ const siteContent = {
 };
 
 // write your code here
+const navContent = document.querySelectorAll("a");
+let k = 0; let st;
+navContent.forEach(function(item){
+  st = "nav-item-"+k;
+  item.textContent = siteContent.nav[st];
+  item.style.color = 'green'; //changing color of text to green
+  k++;
+});
+
+document.getElementById("logo-img").src = siteContent.nav["img-src"];
+
+document.querySelector('.cta-text > h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta-text > button').textContent = siteContent.cta.button;
+document.getElementById("cta-img").src = siteContent.cta["img-src"];
+
+let top_h = document.querySelectorAll('.top-content .text-content > h4');
+let top_p = document.querySelectorAll('.top-content .text-content > p');
+top_h[0].textContent = siteContent["main-content"]["features-h4"];
+top_p[0].textContent = siteContent["main-content"]["features-content"];
+top_h[1].textContent = siteContent["main-content"]["about-h4"];
+top_p[1].textContent = siteContent["main-content"]["about-content"];
+
+document.getElementById("middle-img").src = siteContent["main-content"]["middle-img-src"];
+
+let bottom_h = document.querySelectorAll('.bottom-content .text-content > h4');
+let bottom_p = document.querySelectorAll('.bottom-content .text-content > p');
+bottom_h[0].textContent = siteContent["main-content"]["services-h4"];
+bottom_p[0].textContent = siteContent["main-content"]["services-content"];
+bottom_h[1].textContent = siteContent["main-content"]["product-h4"];
+bottom_p[1].textContent = siteContent["main-content"]["product-content"];
+bottom_h[2].textContent = siteContent["main-content"]["vision-h4"];
+bottom_p[2].textContent = siteContent["main-content"]["vision-content"];
+
+document.querySelector('.contact > h4').textContent = siteContent.contact["contact-h4"];
+let contact_p = document.querySelectorAll('.contact > p');
+contact_p[0].textContent = siteContent.contact.address;
+contact_p[1].textContent = siteContent.contact.phone;
+contact_p[2].textContent = siteContent.contact.email;
+
+document.querySelector('footer > p').textContent = siteContent.footer.copyright;
+
+//adding two items to the nav bar
+let para = document.createElement("a");
+let node = document.createTextNode("New1");
+para.appendChild(node);
+para.style.color = 'green';
+let nav_items = document.querySelector("nav");
+nav_items.appendChild(para);
+
+let paraa = document.createElement("a");
+let nodee = document.createTextNode("New2");
+paraa.appendChild(nodee);
+paraa.style.color = 'green';
+nav_items.appendChild(paraa);
+
+//Stretch goals
+let button = document.querySelector('.cta-text > button');
+button.addEventListener("click",function(){
+  button.style.color = 'pink';
+});
